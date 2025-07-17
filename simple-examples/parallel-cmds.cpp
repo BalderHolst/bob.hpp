749f9e38eaff59092799eb7a2773d5b5de8fc5fe
Balder W. Holst <balderwh@gmail.com>
2025-07-17 18:33:26 +0200

    add simple example 'parallel-cmds.cpp'

---
+++ simple-examples/parallel-cmds.cpp
@@ -0,0 +1,33 @@
+#include "../bob.hpp"
+#include <cstdlib>
+#include <unistd.h>
+
+using namespace bob;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    go_rebuild_yourself(argc, argv);
+
+    auto runner = CmdRunner(10);
+
+    for (int i = 0; i <= 100; ++i) {
+
+        auto random = [] () {
+            return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        };
+
+        string script = "import time;"
+                        "print('Job " + to_string(i) + " started...'); "
+                        "time.sleep(" + to_string(random()) + "); "
+                        "print('Job + " + to_string(i) + " finished!')";
+
+        auto cmd = Cmd({"python3", "-c", script});
+
+        runner.push(cmd);
+    }
+
+    runner.run();
+
+    return EXIT_SUCCESS;
+}
