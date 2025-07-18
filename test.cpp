0b73ce7a5fb23f4c1bcc5548790611a78c9f0457
Balder W. Holst <balderwh@gmail.com>
2025-07-18 16:35:03 +0200

    Add filter to test script

--- test.cpp
+++ test.cpp
@@ -13,13 +13,29 @@ enum class Action {
     Replay,
 };
 
-int run(Action action) {
-
-    vector<path> test_cases;
+vector<path> find_test_cases() {
+    vector<path> cases;
     for (const auto &entry : fs::directory_iterator(TEST_DIR)) {
         path test_case = entry.path();
-        if (!fs::is_directory(test_case)) continue;
+        if (fs::is_directory(test_case)) {
+            cases.push_back(test_case);
+        }
+    }
+    return cases;
+}
+
+int run(Action action, path test_case = "") {
+
+    vector<path> test_cases;
+    if (!test_case.empty()) {
         test_cases.push_back(test_case);
+    }
+    else {
+        test_cases = find_test_cases();
+        if (test_cases.empty()) {
+            cerr << "No test cases found in " << TEST_DIR << endl;
+            return EXIT_FAILURE;
+        }
     }
 
     // Compile all test binaries
@@ -53,8 +69,28 @@ int main(int argc, char* argv[]) {
         return run(Action::Replay);
     });
 
-    cli.add_command("record", "Record tests", [](CliCommand &cmd) { return run(Action::Record); });
-    cli.add_command("replay", "Replay tests", [](CliCommand &cmd) { return run(Action::Replay); });
+    CliCommand &record = cli.add_command("record", "Record tests", [](CliCommand &cmd) {
+        cmd.handle_help();
+        return run(Action::Record);
+    });
+
+
+    CliCommand &replay = cli.add_command("replay", "Replay tests", [](CliCommand &cmd) {
+        cmd.handle_help();
+        return run(Action::Replay);
+    });
+
+    for (const auto &test_case : find_test_cases()) {
+        string test_name = test_case.filename().string();
+        record.add_command(test_name, "Record test case: " + test_name, [test_case](CliCommand &cmd) {
+            cmd.handle_help();
+            return run(Action::Record, test_case);
+        });
+        replay.add_command(test_name, "Replay test case: " + test_name, [test_case](CliCommand &cmd) {
+            cmd.handle_help();
+            return run(Action::Replay, test_case);
+        });
+    }
 
     return cli.serve();
 }
