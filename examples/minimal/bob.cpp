b8c390ad4337225ffde317d3f348bc6948a515aa
Balder W. Holst <balderwh@gmail.com>
2025-08-04 16:02:24 +0200

    Clarifying comment

--- examples/minimal/bob.cpp
+++ examples/minimal/bob.cpp
@@ -8,7 +8,8 @@ const string CPP           = "g++";
 const vector<string> FLAGS = {"-Wall", "-Wextra", "-O2"};
 
 int main(int argc, char *argv[]) {
-    GO_REBUILD_YOURSELF(argc, argv);
+    GO_REBUILD_YOURSELF(argc, argv); // Recompile if the source files have changed
+
     return Cmd({CPP, SRC / "main.cpp", SRC / "add.cpp", "-o", "main"})
         .push_many(FLAGS)
         .run();
